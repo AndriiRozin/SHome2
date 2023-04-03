d607aadcbc60c9e71924afde5ef3eec86c040879
AndriiRozin <andrii.rozin@dxc.com>
2023-04-04 00:46:34 +0300

    update all files to commit

--- SHomeConfigurator/SHConfigurator/mainwindow.cpp
+++ SHomeConfigurator/SHConfigurator/mainwindow.cpp
@@ -1,15 +1,70 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDebug>
+#include <QMessageBox>
+#include <QStatusBar>
+
+#include "Settings/dialog_net_setting.h"
+#include "Settings/dialog_placement_setting.h"
+#include "Settings/dialog_actuator_setting.h"
+#include "Settings/dialog_sensor_setting.h"
+#include "Settings/dialog_signal_setting.h"
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
+    , pSettingContainers(new Setting_Containers)
+    , pDialogSaSetting(new Dialog_SAsetting(this))
+
 {
     ui->setupUi(this);
+
+    //pDialogSaSetting->setModal(false);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_pushButton_Net_clicked()
+{
+    Dialog_net_setting mDialogNet(this, pSettingContainers);
+    mDialogNet.setModal(true);
+    mDialogNet.exec();
+}
+
+
+void MainWindow::on_pushButton_Placement_clicked()
+{
+    Dialog_Placement_Setting mDialogPlacement(this, pSettingContainers);
+    mDialogPlacement.setModal(true);
+    mDialogPlacement.exec();
+}
+
+
+void MainWindow::on_pushButton_Actuator_clicked()
+{
+    Dialog_Actuator_Setting  mDialogActuator(this, pSettingContainers);
+    mDialogActuator.setModal(true);
+    mDialogActuator.exec();
+}
+
+
+void MainWindow::on_pushButton_Sensor_clicked()
+{
+    Dialog_Sensor_Setting mDialogSensor(this, pSettingContainers);
+    mDialogSensor.setModal(true);
+    mDialogSensor.exec();
+}
+
+
+void MainWindow::on_pushButton_Signals_clicked()
+{
+    Dialog_Signal_Setting mDialogSensor(this, pSettingContainers);
+    mDialogSensor.setModal(true);
+    mDialogSensor.exec();
 }
 
--- SHomeConfigurator/SHConfigurator/mainwindow.h
+++ SHomeConfigurator/SHConfigurator/mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+
+#include "Settings/setting_containers.h"
+
+#include "Settings/dialog_sasetting.h"
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +21,20 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_Net_clicked();
+
+    void on_pushButton_Placement_clicked();
+
+    void on_pushButton_Actuator_clicked();
+
+    void on_pushButton_Sensor_clicked();
+
+    void on_pushButton_Signals_clicked();
+
 private:
     Ui::MainWindow *ui;
+    Setting_Containers *  pSettingContainers;
+    Dialog_SAsetting * pDialogSaSetting;
 };
 #endif // MAINWINDOW_H
